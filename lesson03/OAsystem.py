63f7f6dd5e9cf7a0386996a3598f380b856c6c2c
Grubby <574413418@qq.com>
2025-10-24 16:31:42 +0800

    用LangChain快速构建基于"易速 鲜花"本地知识库的智能问答系统

--- lesson03/OAsystem.py
+++ lesson03/OAsystem.py
@@ -3,91 +3,107 @@ import os
 
 # load api key
 load_dotenv("/Users/grubby/Library/Mobile Documents/com~apple~CloudDocs/PycharmProjects/langChain/langChain_learn/env")
-api_key = os.getenv("API_KEY")
-base_url = os.getenv("BASE_URL")
+api_key = os.getenv("SILICON_FLOW")
+base_url = os.getenv("SILLICON_URL")
 
-# 1.load data
-from langchain_community.document_loaders import PyPDFLoader
-from langchain_community.document_loaders import TextLoader
-from langchain_community.document_loaders import Docx2txtLoader
+# 1.Load 导入Document Loaders
+from langchain_community.document_loaders import PyPDFLoader,Docx2txtLoader,TextLoader
 
-# file document path
+
+# 加载Documents
 base_dir = os.path.join(os.path.dirname(__file__), "oneFlowers")
+
 documents = []
-
 for file in os.listdir(base_dir):
-    # build whole file path
-    file_path = os.path.join(base_dir,file)
-
-    if file.endswith(".doc"):
-        loader = Docx2txtLoader(file_path)
-        documents.append(loader.load())
-    elif file.endswith(".pdf"):
+    # 构建完整的文件路径
+    file_path = os.path.join(base_dir, file)
+    if file.endswith('.pdf'):
         loader = PyPDFLoader(file_path)
-        documents.append(loader.load())
-    elif file.endswith(".text"):
+        documents.extend(loader.load())
+    elif file.endswith('.docx'):
+        loader = Docx2txtLoader(file_path)
+        documents.extend(loader.load())
+    elif file.endswith('.txt'):
         loader = TextLoader(file_path)
-        documents.append(loader.load())
+        documents.extend(loader.load())
 
 
-# 2.split text
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-# text has been divide into 200 shots
-text_spliter = RecursiveCharacterTextSplitter(chunk_size=200,chunk_overlap=10)
+# 2.Split 将Documents切分成块以便后续进行嵌入和向量存储
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=350, chunk_overlap=50)
+chunked_documents = text_splitter.split_documents(documents)
 
 
-flat_documents = [doc for sublist in documents for doc in sublist]
-
-# using text_spliter to slit text
-chunked_documents = text_spliter.split_documents(flat_documents)
-
-# 3.Store Embed and segment the data and store it in the vector database Qdrant.
+# 3.Store 将分割嵌入并存储在矢量数据库Qdrant中
 from langchain_community.vectorstores import Qdrant
+from langchain_openai import OpenAIEmbeddings
+from typing import List, Optional
 
-from langchain.pydantic_v1 import BaseModel
-from langchain.embeddings.base import Embeddings
-
-class DoubaoEmbeddings(BaseModel, Embeddings):
-    client: Ark = None
-    api_key: str = ""
-    model: str
-
-    def __init__(self, **data: Any):
-        super().__init__(**data)
-        if self.api_key == "":
-            self.api_key = os.environ["OPENAI_API_KEY"]
-        self.client = Ark(
-            base_url=os.environ["OPENAI_BASE_URL"],
-            api_key=self.api_key
-        )
-
-    def embed_query(self, text: str) -> List[float]:
-        """
-        Generate the embedding of the input text.
-        Args:
-            texts (str): to generate embedding text
-        Return:
-            embeddings (List[float]): embedding，a float value list.
-        """
-        embeddings = self.client.embeddings.create(model=self.model, input=text)
-        return embeddings.data[0].embedding
-
-    def embed_documents(self, texts: List[str]) -> List[List[float]]:
-        return [self.embed_query(text) for text in texts]
-
-    class Config:
-        arbitrary_types_allowed = True
-
-
-vectorstore = Qdrant.from_documents(
-    # splited document
-    documents=chunked_documents,
-    embedding=DoubaoEmbeddings(
-        model=os.environ["EMBEDDING_MODELEND"],
-    ),
-    # using OpenAI`s Embedding Model
-    location=":memory:",
-    # in-memory
-    collection_name="my_documents",
+embedding = OpenAIEmbeddings(
+    api_key = api_key,
+    base_url = base_url,
+    model="BAAI/bge-large-zh-v1.5"
 )
 
+vectorstore = Qdrant.from_documents(
+    documents=chunked_documents, # 以分块的文档
+    embedding=embedding, # 用OpenAI的Embedding Model做嵌入
+    location=":memory:",  # in-memory 存储
+    collection_name="my_documents",# 指定collection_name
+    batch_size = 16
+)
+
+# 4. Retrieval 准备模型和Retrieval链
+import logging # 导入Logging工具
+from langchain_openai import ChatOpenAI # ChatOpenAI模型
+from langchain_classic.retrievers import MultiQueryRetriever# MultiQueryRetriever工具
+from langchain_classic.chains import RetrievalQA # RetrievalQA链
+
+
+
+# 设置Logging
+logging.basicConfig()
+logging.getLogger('langchain.retrievers.multi_query').setLevel(logging.INFO)
+
+deepbricks_api = os.getenv("API_KEY")
+deepbricks_url = os.getenv("BASE_URL")
+
+# 实例化一个大模型工具
+llm = ChatOpenAI(
+    api_key = deepbricks_api,
+    base_url = deepbricks_url,
+    model_name="gemini-2.5-flash",
+    temperature=0.5
+)
+
+# 实例化一个MultiQueryRetriever
+retriever_from_llm = MultiQueryRetriever.from_llm(retriever=vectorstore.as_retriever(), llm=llm)
+
+# 实例化一个RetrievalQA链
+qa_chain = RetrievalQA.from_chain_type(llm,retriever=retriever_from_llm)
+
+
+# 5. Output 问答系统的UI实现
+from flask import Flask, request, render_template
+
+app = Flask(__name__)  # Flask APP
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        # 接收用户输入作为问题
+        question = request.form.get('question')
+
+        # RetrievalQA链 - 读入问题，生成答案
+        result = qa_chain({"query": question})
+
+        # 把大模型的回答结果返回网页进行渲染
+        return render_template('index.html', result=result)
+
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True, port=8000)
